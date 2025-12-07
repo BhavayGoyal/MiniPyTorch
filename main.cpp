dbe67251015128e6892404e98fdd6881280adf4c
Bhavay Goyal <bhavaygoyal4b@gmail.com>
2025-12-07 21:17:02 +0530

    Random

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+import temp;
+using namespace std;
+
+int main() {
+    int a = 5, b = 10;
+    int ans = add(a, b);
+
+    cout << ans << endl;
+    return 0;
+}
---
+++ temp.cppm
@@ -0,0 +1,5 @@
+export module temp;
+
+export int add(int a, int b) {
+    return a + b;
+}
